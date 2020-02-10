88ecaa5ba6d93f736dc0974f2ab6ca5e9adea611
KING RIO BIRD <ryomatakenaga@gmail.com>
2020-02-09 23:07:47 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function init() {
     victoryAudio.pause();
     victoryAudio.currentTime = 0;
 }
-//this is for 
+//this is for the 
 function render() {
     board.forEach(function(mark, index) {
         squares[index].textContent = mark;
